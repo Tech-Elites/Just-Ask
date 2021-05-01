dc9d548d090e43e1d4ddf9b31321a15e2307a156
harshva89 <harshvaswani89@gmail.com>
2021-05-01 10:32:14 +0530

    login

--- app/src/main/java/com/example/just_ask/LoginPage.java
+++ app/src/main/java/com/example/just_ask/LoginPage.java
@@ -1,14 +1,117 @@
 package com.example.just_ask;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ActivityOptions;
+import android.app.AlertDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class LoginPage extends AppCompatActivity {
 
+    String email,password;
+    FirebaseAuth firebaseAuth;
+    ProgressBar p;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_page);
+        firebaseAuth = FirebaseAuth.getInstance();
+        p=findViewById(R.id.progressBarLogin);
+        p.setVisibility(View.INVISIBLE);
+
     }
+
+    public void loginPageLogin(View view) {
+        EditText t1 = findViewById(R.id.loginPageEmail);
+        EditText t2 = findViewById(R.id.loginPagePassword);
+        email = t1.getText().toString();
+        password = t2.getText().toString();
+        login(email, password);
+    }
+
+    void login(String email, String password){
+        if(TextUtils.isEmpty(email) || TextUtils.isEmpty(password))
+        {
+            new AlertDialog.Builder(this)
+                    .setIcon(android.R.drawable.ic_dialog_info)
+                    .setTitle("Details not filled!")
+                    .setMessage("Fill all the details to proceed.")
+                    .setPositiveButton("Ok",null)
+                    .show();
+        }
+        else
+        {
+            p.setVisibility(View.VISIBLE);
+            firebaseAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if(task.isSuccessful())
+                    {
+                        FirebaseUser u= firebaseAuth.getCurrentUser();
+                        if(u!=null)
+                        {
+                            DatabaseReference databaseReference= FirebaseDatabase.getInstance().getReference().child("teachers");
+                            databaseReference.addValueEventListener(new ValueEventListener() {
+                                @Override
+                                public void onDataChange(@NonNull DataSnapshot snapshot) {
+                                    int flag=1;
+                                    for(DataSnapshot dataSnapshot:snapshot.getChildren())
+                                    {
+                                        if(u.getUid().compareTo(dataSnapshot.getKey())==0)
+                                        {
+                                            p.setVisibility(View.INVISIBLE);
+                                            Intent i = new Intent(LoginPage.this, TeacherLanding.class);
+                                            i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+                                            startActivity(i, ActivityOptions.makeSceneTransitionAnimation(LoginPage.this).toBundle());
+                                            flag=0;
+                                            break;
+                                        }
+                                    }
+                                    if(flag==1)
+                                    {
+                                        Intent i = new Intent(LoginPage.this, StudentLanding.class);
+                                        i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+                                        startActivity(i, ActivityOptions.makeSceneTransitionAnimation(LoginPage.this).toBundle());
+                                    }
+                                }
+
+                                @Override
+                                public void onCancelled(@NonNull DatabaseError error) {
+
+                                }
+                            });
+                        }
+                    }
+                    else
+                    {
+                        new AlertDialog.Builder(LoginPage.this)
+                                .setIcon(android.R.drawable.ic_dialog_alert)
+                                .setTitle("Error logging in.")
+                                .setMessage("Invalid login credentials. Please check your credentials and try again.")
+                                .setPositiveButton("Yes",null)
+                                .show();
+                    }
+                }
+            });
+        }
+
+    }
+
 }
--- app/src/main/java/com/example/just_ask/MainActivity.java
+++ app/src/main/java/com/example/just_ask/MainActivity.java
@@ -3,7 +3,11 @@ package com.example.just_ask;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ActivityOptions;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
@@ -15,26 +19,52 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
-
-    FirebaseUser user;
+    ProgressBar p;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
+        p=findViewById(R.id.mainProgress);
+        FirebaseUser u = FirebaseAuth.getInstance().getCurrentUser();
+        if (u != null) {
+            DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference().child("teachers");
+            databaseReference.addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot snapshot) {
+                    int flag = 1;
+                    for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
+                        if (u.getUid().compareTo(dataSnapshot.getKey()) == 0) {
+                            flag=0;
+                            p.setVisibility(View.INVISIBLE);
+                            Intent i = new Intent(MainActivity.this, TeacherLanding.class);
+                            i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+                            startActivity(i, ActivityOptions.makeSceneTransitionAnimation(MainActivity.this).toBundle());
 
-        user= FirebaseAuth.getInstance().getCurrentUser();
-        DatabaseReference databaseReference= FirebaseDatabase.getInstance().getReference().child("teachers");
-        databaseReference.addListenerForSingleValueEvent(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot snapshot) {
-                Toast.makeText(MainActivity.this, snapshot.getValue().toString(), Toast.LENGTH_SHORT).show();
-            }
+                            break;
+                        }
+                    }
+                    if (flag == 1) {
+                        Intent i = new Intent(MainActivity.this, StudentLanding.class);
+                        i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+                        startActivity(i, ActivityOptions.makeSceneTransitionAnimation(MainActivity.this).toBundle());
 
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
+                    }
+                }
 
-            }
-        });
+                @Override
+                public void onCancelled(@NonNull DatabaseError error) {
+
+                }
+            });
+        }
+        else
+        {
+            Intent i = new Intent(MainActivity.this, LoginPage.class);
+            i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+            startActivity(i, ActivityOptions.makeSceneTransitionAnimation(MainActivity.this).toBundle());
+
+        }
 
     }
 }
