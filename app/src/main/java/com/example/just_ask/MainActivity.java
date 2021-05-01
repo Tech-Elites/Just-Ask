65abfb12a3c9e50897196b6f0c9718684875383d
harshva89 <harshvaswani89@gmail.com>
2021-05-01 10:06:58 +0530

    First2

--- app/src/main/java/com/example/just_ask/MainActivity.java
+++ app/src/main/java/com/example/just_ask/MainActivity.java
@@ -1,14 +1,40 @@
 package com.example.just_ask;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
 
+    FirebaseUser user;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        user= FirebaseAuth.getInstance().getCurrentUser();
+        DatabaseReference databaseReference= FirebaseDatabase.getInstance().getReference().child("teachers");
+        databaseReference.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                Toast.makeText(MainActivity.this, snapshot.getValue().toString(), Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
     }
 }
---
+++ app/src/main/java/com/example/just_ask/StudentLanding.java
@@ -0,0 +1,14 @@
+package com.example.just_ask;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class StudentLanding extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_student_landing);
+    }
+}
